8e4e4903875a200fed348bdabc729ceb7100f730
salam0smy <salam0smy@gmail.com>
2014-09-09 03:31:16 -0400

    added dependencies

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
-var app = express();
 var mongoose = require('mongoose');
+var fs = require('fs');
+var _ = require('underscore'),
+	passport = require('passport'),
+	icalendar = require('icalendar');
+var gcal = require('google-calendar');
+
 var USER = process.env.MASTER_USERNAME;
 var PASS = process.env.MASTER_PASSWORD;
+
+var app = express();
 var auth = express.basicAuth(USER, PASS);
 // connect to the database
 var db = mongoose.connect(process.env.MONGOLAB_URI);
@@ -12,13 +19,13 @@ mongoose.connection.once('connected', function() {
 	console.log("Connected to database");
 });
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+  response.send('Hello World!');
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
