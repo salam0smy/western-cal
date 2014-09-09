cf5341b2c3a4c938ff7084a8da586346fdc09d90
salam0smy <salam0smy@gmail.com>
2014-09-09 02:57:46 -0400

    connect to db

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var express = require('express')
 var app = express();
+var mongoose = require('mongoose');
+
+// connect to the database
+var db = mongoose.connect(process.env.MONGOLAB_URI);
+
+//attach lister to connected event
+mongoose.connection.once('connected', function() {
+	console.log("Connected to database");
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
